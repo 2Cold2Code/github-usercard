2188060539b45a63e606febf46d8679b81d32ac8
Adam Osburn <wizardofosburn@gmail.com>
2020-06-11 22:23:39 -0400

    removed excess axios call

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,12 +1,4 @@
-axios.get('https://api.github.com/users/2cold2code')
-     .then(response => {
-       console.log(response)
-       const {data} = response;
-        }
-      )
-     .catch(err => console.log(err.errors))
-
-     /*
+/*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
@@ -67,14 +59,16 @@ const followersArray = [];
     bigknell
 */
 
-
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
 
-function gitByUser(usrnm){
-  axios.get(`https://api.github.com/users/${usrnm}`).then(response => userCards(response)).catch(e => console.log(e))
+function gitByUser(usrnm) {
+  axios
+    .get(`https://api.github.com/users/${usrnm}`)
+    .then((response) => userCards(response))
+    .catch((e) => console.log(e));
 }
 const me = "2Cold2Code";
 const myGit = gitByUser(me);
@@ -125,14 +119,14 @@ let followersArray = [];
 */
 
 function userCards(gitObj) {
-  const {data} = gitObj;
-  
+  const { data } = gitObj;
+
   const cards = document.querySelector(".cards");
-  
+
   const card = document.createElement("div");
   card.classList.add("card");
   cards.appendChild(card);
-  
+
   const img = document.createElement("img");
   img.setAttribute("src", data["avatar_url"]);
   card.appendChild(img);
@@ -140,44 +134,45 @@ function userCards(gitObj) {
   const cardInfo = document.createElement("div");
   cardInfo.classList.add("card-info");
   card.appendChild(cardInfo);
-  
+
   const name = document.createElement("h3");
   name.classList.add("name");
   name.textContent = data["name"];
   cardInfo.appendChild(name);
-  
+
   const userName = document.createElement("p");
   userName.classList.add("username");
   userName.textContent = data["login"];
   cardInfo.appendChild(userName);
-  
+
   const location = document.createElement("p");
   location.textContent = `Location: ${data["location"]}`;
   cardInfo.appendChild(location);
-  
+
   const profile = document.createElement("p");
   cardInfo.appendChild(profile);
-  
+
   const profileLink = document.createElement("a");
   profileLink.href = data["url"];
   profileLink.textContent = `Profile: ${profileLink.href}`;
-  
+
   profile.appendChild(profileLink);
-  
+
   const followers = document.createElement("p");
   followers.textContent = `Followers: ${data["followers"]}`;
   cardInfo.appendChild(followers);
-  
+
   const followerURL = data["followers_url"];
-  gitByUser(followerURL).then((obj) => {
-    obj.data.forEach((follower) => followersArray.push(follower.url))
-  })
-  .catch(error => console.log('Error \u{498f3}'));
-  
+  gitByUser(followerURL)
+    .then((obj) => {
+      obj.data.forEach((follower) => followersArray.push(follower.url));
+    })
+    .catch((error) => console.log("Error \u{498f3}"));
+
   const following = document.createElement("p");
   following.textContent = `Following: ${data["following"]}`;
   cardInfo.appendChild(following);
-  
+
   const bio = document.createElement("p");
   bio.textContent = `Bio: ${data["bio"]}`;
   cardInfo.appendChild(bio);
@@ -188,3 +183,26 @@ function userCards(gitObj) {
 myGit
   .then((response) => userCards(response))
   .catch((err) => console.log("error \u{02970}\u{02964} ", err));
+
+myGit
+  .then((myGitObject) => gitByUser(myGitObject.data.followers_url))
+  .then((urls) =>
+    urls.data.forEach((URL) =>
+      gitByUser(URL.url)
+        .then((usernameURL) => {
+          userCards(usernameURL);
+          followersArray.forEach((name) =>
+            gitByUser(name)
+              .then((nameObj) => userCards(nameObj.data))
+              .catch((error) => console.log("error \u{0f98a}"))
+          );
+        })
+        .catch((e) => console.log(e))
+    )
+  )
+  .catch((err) => console.log(err))
+  .catch((e) => console.log("inner error: \u{04f348} ", e))
+  .catch((e) => console.log("Error \u{02970}\u{02964}", e));
+
+let TLs = ["dustinmyers", "tetondan", "justsml", "luishrd", "bigknell"];
+TLs.forEach((tl) => gitByUser(tl));
